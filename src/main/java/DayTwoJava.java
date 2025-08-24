a6306f1a8d297397d1cf03327a80e227b5d9669e
Pavan453 <97392496+Pavan453@users.noreply.github.com>
2025-08-24 11:03:13 +0530

    Update DayTwoJava.java

--- src/main/java/DayTwoJava.java
+++ src/main/java/DayTwoJava.java
@@ -2,5 +2,6 @@ public class DayTwoJava {
     public static void main(String[] args) {
         System.out.println("Hello IntelliJ!");
         System.out.println("daythree");
+        System.out.println("daythree Git hub edited");
     }
 }
