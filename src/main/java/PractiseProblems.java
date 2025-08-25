eb41dbb92950dfd3379a427904d6d086989d5900
Pavan kumar <k.pavankumar34@gmail.com>
2025-08-25 23:07:51 +0530

    monday

--- src/main/java/PractiseProblems.java
+++ src/main/java/PractiseProblems.java
@@ -2,12 +2,47 @@ import java.util.Scanner;
 
 public class PractiseProblems {
 
-        public static void main(String[]  args){
-            Scanner obj = new Scanner(System.in);
-            int a = obj.nextInt();
-            int b = obj.nextInt();
-            int c = obj.nextInt();
-            int sum = a+b+c;
-            System.out.println(sum);
+        public static void main(String[]  args) {
+
+        PractiseProblems p1=new PractiseProblems();
+        p1.age(4,"female");
+
         }
-    }
+
+        public void age(int age, String gender) {
+
+            if ((age>0) && (age<18) ) {
+
+                if(gender=="male") {
+                    System.out.println("boy");
+
+
+                }
+                else {
+                    System.out.println("girl");
+                }
+            }
+
+            else if ((age>=18) && (age<60)){
+                if(gender=="female"){
+                    System.out.println("woman");
+                }
+
+                else{
+                    System.out.println("man");
+                }
+            }
+
+            else if (age>=60) {
+
+                    System.out.println("scenior citizen");
+                }
+
+            else {
+
+                System.out.println("age should be proper");
+            }
+
+            }
+        }
+
